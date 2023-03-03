af31debbb8c1aa5135fc890ef8f441416516c3aa
ccadran <clariocadran33@gmail.com>
2023-03-03 09:46:19 +0100

    test

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,28 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    purge: {
+        enabled: process.env.NODE_ENV === "production",
+        content: [
+            "./public/**/*.html",
+            "./src/**/*.{js,jsx,ts,tsx,vue}",
+            "./app/**/*.html.erb",
+            "./app/**/*.html.haml",
+            "./app/**/*.html.slim",
+            "./app/**/*.js",
+        ],
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    variants: {},
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/typography"),
+    ],
+};
