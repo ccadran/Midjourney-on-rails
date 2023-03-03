8423e25ff5e28fb8dde0a38cf5e2624c6bdd62ea
ccadran <clariocadran33@gmail.com>
2023-03-03 09:53:21 +0100

    test

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,28 +1,28 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+// const defaultTheme = require("tailwindcss/defaultTheme");
 
-module.exports = {
-    purge: {
-        enabled: process.env.NODE_ENV === "production",
-        content: [
-            "./public/**/*.html",
-            "./src/**/*.{js,jsx,ts,tsx,vue}",
-            "./app/**/*.html.erb",
-            "./app/**/*.html.haml",
-            "./app/**/*.html.slim",
-            "./app/**/*.js",
-        ],
-    },
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-    variants: {},
-    plugins: [
-        require("@tailwindcss/forms"),
-        require("@tailwindcss/aspect-ratio"),
-        require("@tailwindcss/typography"),
-    ],
-};
+// module.exports = {
+//     purge: {
+//         enabled: process.env.NODE_ENV === "production",
+//         content: [
+//             "./public/**/*.html",
+//             "./src/**/*.{js,jsx,ts,tsx,vue}",
+//             "./app/**/*.html.erb",
+//             "./app/**/*.html.haml",
+//             "./app/**/*.html.slim",
+//             "./app/**/*.js",
+//         ],
+//     },
+//     theme: {
+//         extend: {
+//             fontFamily: {
+//                 sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+//             },
+//         },
+//     },
+//     variants: {},
+//     plugins: [
+//         require("@tailwindcss/forms"),
+//         require("@tailwindcss/aspect-ratio"),
+//         require("@tailwindcss/typography"),
+//     ],
+// };
